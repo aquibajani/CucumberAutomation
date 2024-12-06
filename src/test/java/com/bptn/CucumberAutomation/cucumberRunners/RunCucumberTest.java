a6ad280ba28eb0b567ea42579c8defe01e3bd44a
Aquib Ajani <aquibajani@Aquibs-MacBook-Pro.local>
2024-12-06 10:24:48 -0500

    Example Scenario Feature

---
+++ src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package com.bptn.CucumberAutomation.cucumberRunners;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com/bptn/CucumberAutomation")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bptn.CucumberAutomation")
+public class RunCucumberTest {
+}
