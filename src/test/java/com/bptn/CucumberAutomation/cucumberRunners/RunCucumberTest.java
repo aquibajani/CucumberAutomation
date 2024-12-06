66099d435b61a198820bc4f5de401e604519101d
Aquib Ajani <aquibajani@Aquibs-MacBook-Pro.local>
2024-12-06 13:36:25 -0500

    Login Feature:

--- src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
+++ src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
@@ -10,10 +10,11 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("com/bptn/CucumberAutomation/features/example.feature")
-@SelectClasspathResource("com/bptn/CucumberAutomation/features/calculator.feature")
-@SelectClasspathResource("com/bptn/CucumberAutomation/features/strongPassword.feature")
-@SelectClasspathResource("com/bptn/CucumberAutomation/features/signUp.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/features/example.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/features/calculator.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/features/strongPassword.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/features/signUp.feature")
+@SelectClasspathResource("com/bptn/CucumberAutomation/features/login.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bptn.CucumberAutomation")
 public class RunCucumberTest {
 }
