c7ebc8c6f7676de7673aa8b75a53e8d08db3c52d
Aquib Ajani <aquibajani@Aquibs-MacBook-Pro.local>
2024-12-06 11:45:35 -0500

    Signup Feature

--- src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
+++ src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
@@ -13,6 +13,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("com/bptn/CucumberAutomation/features/example.feature")
 @SelectClasspathResource("com/bptn/CucumberAutomation/features/calculator.feature")
 @SelectClasspathResource("com/bptn/CucumberAutomation/features/strongPassword.feature")
+@SelectClasspathResource("com/bptn/CucumberAutomation/features/signUp.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bptn.CucumberAutomation")
 public class RunCucumberTest {
 }
